71dc131b78039a5fbd5212fa935db5c6254e2673
nino Gonzalez <ggnino18@gmail.com>
2022-10-06 10:12:15 -0400

    fixing build

--- client/src/components/tour-prev-component/tour-prev-component.jsx
+++ client/src/components/tour-prev-component/tour-prev-component.jsx
@@ -41,7 +41,6 @@ function TourPrev() {
 	});
 	// useEffect hook for retrieving tour data and observing component
 	useEffect(() => {
-		console.log(threshold);
 		// Chechking to see if component in user view
 		if (inView) {
 			setMainRefs((r) => {
@@ -90,65 +89,67 @@ function TourPrev() {
 			</div>
 
 			<div className="tour-prev-content">
-				{tours.map((tour, index) => {
-					if (
-						(index === 0 && tour.difficulty === 'easy') ||
-						(index === 1 && tour.difficulty === 'medium') ||
-						(index === 3 && tour.difficulty === 'difficult')
-					) {
-						return (
-							<Link
-								className="tour-prev-content"
-								to={`/tours/${tour.slug}`}
-								state={tour}
-								key={`${tour.name} ${index}`}
-							>
-								<div
-									key={`${tour.name}-${index}`}
-									className="tour-prev-content-cards"
-								>
-									<div
-										className="img-holder"
-										id={
-											tour.difficulty === 'easy'
-												? 'one'
-												: tour.difficulty === 'medium'
-												? 'two'
-												: 'three'
-										}
+				{tours
+					? tours.map((tour, index) => {
+							if (
+								(index === 0 && tour.difficulty === 'easy') ||
+								(index === 1 && tour.difficulty === 'medium') ||
+								(index === 3 && tour.difficulty === 'difficult')
+							) {
+								return (
+									<Link
+										className="tour-prev-content"
+										to={`/tours/${tour.slug}`}
+										state={tour}
+										key={`${tour.name} ${index}`}
 									>
-										<img
-											src={`/imgs/tours/${tour.imageCover}`}
-											alt="tour-cover"
-										/>
-										<h2>{tour.name}</h2>
-									</div>
-									<ul>
-										<li>{tour.duration} day tour</li>
-										<li>Up to {tour.maxGroupSize} people</li>
-										<li>{tour.guides.length} tour guides</li>
-										<li>All accomodations included</li>
-										<li>
-											Difficulty:{' '}
-											<span
-												style={
+										<div
+											key={`${tour.name}-${index}`}
+											className="tour-prev-content-cards"
+										>
+											<div
+												className="img-holder"
+												id={
 													tour.difficulty === 'easy'
-														? { color: '#00916e' }
+														? 'one'
 														: tour.difficulty === 'medium'
-														? { color: '#ee6123' }
-														: { color: '#db0000' }
+														? 'two'
+														: 'three'
 												}
 											>
-												{' '}
-												{tour.difficulty}
-											</span>
-										</li>
-									</ul>
-								</div>
-							</Link>
-						);
-					} else return '';
-				})}
+												<img
+													src={`/imgs/tours/${tour.imageCover}`}
+													alt="tour-cover"
+												/>
+												<h2>{tour.name}</h2>
+											</div>
+											<ul>
+												<li>{tour.duration} day tour</li>
+												<li>Up to {tour.maxGroupSize} people</li>
+												<li>{tour.guides.length} tour guides</li>
+												<li>All accomodations included</li>
+												<li>
+													Difficulty:{' '}
+													<span
+														style={
+															tour.difficulty === 'easy'
+																? { color: '#00916e' }
+																: tour.difficulty === 'medium'
+																? { color: '#ee6123' }
+																: { color: '#db0000' }
+														}
+													>
+														{' '}
+														{tour.difficulty}
+													</span>
+												</li>
+											</ul>
+										</div>
+									</Link>
+								);
+							} else return '';
+					  })
+					: ''}
 			</div>
 			<ErrorComponent msg={'OOOPS! TOUR ERROR!'} />
 			<Button msg={'More Tours'} class={'info'} link={'tours'} />
