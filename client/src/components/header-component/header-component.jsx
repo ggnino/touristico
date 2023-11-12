fbfa9241cbadbc1241e1f1f14d1e969f21687da4
Guillermo Gonzalez <ggnino18@gmail.com>
2023-11-12 18:33:48 -0500

    Cleaned Up Header Component Styles
    Removed unnecessary media queries

--- client/src/components/header-component/header-component.jsx
+++ client/src/components/header-component/header-component.jsx
@@ -1,18 +1,18 @@
-import React, { useContext, useEffect, useRef } from 'react';
-import './header-component-styles.scss';
-import img1 from '../../imgs/edge2.mp4';
-import Button from '../button-component/button-component';
-import { MyContext } from '../../utils/functions/context';
-import { useInView } from 'react-intersection-observer';
+import React, { useContext, useEffect, useRef } from "react";
+import "./header-component-styles.scss";
+import img1 from "../../imgs/edge2.mp4";
+import Button from "../button-component/button-component";
+import { MyContext } from "../../utils/functions/context";
+import { useInView } from "react-intersection-observer";
 function HeaderComponent() {
 	// useContext hook for app state
 	const state = useContext(MyContext);
 	// Destructuring state
 	const { setMainRefs } = state;
-	const threshold = useRef(0.25);
+	const threshold = useRef(1);
 	// Adjust threshold by viewport
 	if (window.visualViewport.height > 1400) {
-		threshold.current = 0.2;
+		// threshold.current = 0.2;
 	}
 	// useInView hook for observing component
 	const [ref, inView] = useInView({
@@ -38,18 +38,18 @@ function HeaderComponent() {
 	}, [inView, setMainRefs]);
 	// Render component
 	return (
-		<header role={'banner'} className="container header" ref={ref}>
+		<header role={"banner"} className="container header" ref={ref}>
 			<div className="bg-video">
-				<video src={img1 || ''} autoPlay muted loop></video>
+				<video src={img1 || ""} autoPlay muted loop></video>
 			</div>
 			<h1 className="header-greeting text-gradient">Welcome</h1>
 
 			<h2>Your next adventure awaits!</h2>
 
 			<Button
-				msg={'Discover our tours'}
-				class={'home text-gradient'}
-				link={'#tour-prev'}
+				msg={"Discover our tours"}
+				class={"home text-gradient"}
+				link={"#tour-prev"}
 			/>
 		</header>
 	);
