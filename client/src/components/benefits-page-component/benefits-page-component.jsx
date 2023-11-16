d9abcb4b8c384ca41d4eca50d10c8c8b9de3e141
Guillermo Gonzalez <ggnino18@gmail.com>
2023-11-16 11:32:02 -0500

    Cleaned Up About & Benefit Page Styling

--- client/src/components/benefits-page-component/benefits-page-component.jsx
+++ client/src/components/benefits-page-component/benefits-page-component.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import Button from '../button-component/button-component';
-import './benefits-page-component-styles.scss';
-import img1 from '../../imgs/earn.png';
-import img2 from '../../imgs/earn2.png';
-import img3 from '../../imgs/repeat.png';
-import img5 from '../../imgs/rewards.jpg';
-import img6 from '../../imgs/far.jpg';
+import React from "react";
+import Button from "../button-component/button-component";
+import "./benefits-page-component-styles.scss";
+import img1 from "../../imgs/earn.png";
+import img2 from "../../imgs/earn2.png";
+import img3 from "../../imgs/repeat.png";
+import img5 from "../../imgs/rewards.jpg";
+import img6 from "../../imgs/far.jpg";
 
 function BenefitsPage(props) {
 	// Render component
 	return (
 		<>
-			<div className="benefits-content-info" style={{ display: props.style }}>
+			<div
+				className="benefits-content-info flex flex-col"
+				style={{ display: props.style }}
+			>
 				<h2 className="my-heading">
 					Love traveling? Love saving on traveling more? Say hello to our
 					membership program.
 				</h2>
 				<Button
-					msg={'Touristico Rewards'}
-					class={'home text-gradient'}
-					link={'/signup'}
+					msg={"Touristico Rewards"}
+					class={"home text-gradient"}
+					link={"/signup"}
 				/>
 
 				<p>
 					<br />
-					Get more out of your trips with{' '}
+					Get more out of your trips with{" "}
 					<span className="text-gradient">Touristico Rewards.</span> <br /> Join
 					today to start collecting points, enjoy Member Prices, and be on your
 					way to unlocking membership perks.
@@ -32,10 +35,10 @@ function BenefitsPage(props) {
 			</div>
 
 			<article
-				className="benefits-content-rewards"
+				className="benefits-content-rewards flex"
 				style={{ display: props.style }}
 			>
-				<div className="benefits-content-rewards-reward">
+				<div className="benefits-content-rewards-reward flex flex-col">
 					<img src={img1} alt="" />
 					<h2 className="my-heading">Earn</h2>
 					<p>
@@ -44,7 +47,7 @@ function BenefitsPage(props) {
 					</p>
 				</div>
 
-				<div className="benefits-content-rewards-reward">
+				<div className="benefits-content-rewards-reward flex flex-col ">
 					<img src={img2} alt="" />
 					<h2 className="my-heading">Redeem</h2>
 					<p>
@@ -52,7 +55,7 @@ function BenefitsPage(props) {
 						to save on your next eligible trip.
 					</p>
 				</div>
-				<div className="benefits-content-rewards-reward">
+				<div className="benefits-content-rewards-reward flex flex-col">
 					<img src={img3} alt="" />
 					<h2 className="my-heading">Repeat</h2>
 					<p>
@@ -62,7 +65,7 @@ function BenefitsPage(props) {
 				</div>
 			</article>
 			<article
-				className=" benefits-content-membership"
+				className=" benefits-content-membership flex flex-col"
 				style={{ display: props.style }}
 			>
 				<img src={img5} alt="" />
@@ -76,14 +79,14 @@ function BenefitsPage(props) {
 				</h2>
 			</article>
 			<article
-				className="benefits-content-membership"
+				className="benefits-content-membership flex flex-col"
 				style={{ display: props.style }}
 			>
 				<img src={img6} alt="" />
 				<p>
 					Your rewards keep growing the more you travel and as you reach higher
 					membership tiers you’ll unlock new perks. See what’s in store at each
-					tier level and discover all the{' '}
+					tier level and discover all the{" "}
 					<span className="text-gradient">Touristico Rewards</span>benefits.
 				</p>
 				<h2 className="my-heading">
